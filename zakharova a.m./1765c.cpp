9cf29268ea4c5d0dc29f7fe7f825ce1c0d684d53
ZakharovaAnastasia04 <113917045+ZakharovaAnastasia04@users.noreply.github.com>
2022-10-18 11:49:27 +0300

    Create 1765c.cpp

---
+++ zakharova a.m./1765c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>;
+#include <string>;
+ using namespace std;
+
+int main()
+{
+    int t;
+    string s;
+    cout<<" input a number  t=";
+    cin>>t;  
+   // cout<<"Input a answer ";
+    cin>>s;
+    char* str =s;
+int count = strlen(str);
+cout( count);
+    
+}
