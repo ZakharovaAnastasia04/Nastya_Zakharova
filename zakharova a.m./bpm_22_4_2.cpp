24463c78682de9390b22690da6b1f55d9b02f143
ZakharovaAnastasia04 <113917045+ZakharovaAnastasia04@users.noreply.github.com>
2022-12-20 12:07:42 +0300

    Create bpm_22_4_2.cpp

---
+++ zakharova a.m./bpm_22_4_2.cpp
@@ -0,0 +1,30 @@
+//Number 2
+#include <iostream>;
+#include <string>;
+ //Сигнатура 
+ //struct Okr{
+//double x;
+//std::cin>>x>>std::endl;
+//double y;
+//std::cin>>y>>std::endl;
+//return x*x+y*y<=1;
+//};
+
+int main()
+{
+   int x1;
+   std::cin>> x1 >>std::endl;
+   int y1;
+   std::cin>> y1 >>std::endl;
+   double x;
+   double y;
+    x=0;
+    y=0;
+    bool n = (y1>x-1)&&(y1<x-1)&&(x*x+y*y<=1) ;
+   if n= true {
+    std::cout<<x1;
+    std::cout<<y1
+    else {
+        cout<<"Write another number for x1 and y1"<<std::endl;
+    }
+};
